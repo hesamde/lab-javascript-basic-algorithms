4fb29720a92412eaecdac576e1d3cf9deaa57a11
hesamde <74820651+hesamde@users.noreply.github.com>
2023-03-25 16:05:13 +0100

    done-home-work-25-03-2023

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log(`The driver´s name is ${hacker1}`);
 let hacker2 = "Tyrion"
 console.log(`The driver´s name is ${hacker2}`);
 
-// Iteration 2:
+// Iteration 2: 
 
 if ((hacker1.length) > (hacker2.length)) {
     console.log("The driver has the longest name, it has " + hacker1.length + " " + characters)
